6ac38f868ece8dd89fd0fd5a9f9cd284c35ec0e1
nijimajohn <hirokikotera0211@gmail.com>
2021-10-18 21:00:48 +0900

    [追加]headerデザイン

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import CoverImage from "../images/cover-image.jpeg"
+import ProfileImage from "../images/profile-image.png"
+
+export const Header = () => {
+  return (
+    <header className="main-cover" style={{backgroundImage: `url(${CoverImage})`}}>
+    <div className="overlay" ></div>
+    <div className="container">
+      <div className="display-table" >
+        <div className="display-table-contents">
+        <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
+        <h1 className="title-text">小寺 浩揮</h1>
+        <h3 className="title-text">学生</h3>
+        <ul className="social-icons">
+          <li className="icon-link">
+
+          </li>
+          <li className="icon-link">
+
+          </li>
+        </ul>
+      </div>
+    </div>
+    </div>
+    </header>
+  );
+};
