321600f13c27c3d01d7cb532283a76184f780ed5
nijimajohn <hirokikotera0211@gmail.com>
2021-10-26 15:29:21 +0900

    [デプロイ]

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,8 @@ export const Contact = () => {
       <div className="container">
         <div className="contact-contents">
           <h2>Contact to Me!</h2>
-          <p className="description">お仕事のご相談・ご依頼など下記よりご連絡ください。</p>
-          <a href="https://twitter.com/" className="btn">Contact Us</a>
+          <p className="description">ご相談・ご連絡など下記よりご連絡ください。</p>
+          <p>hirokikotera0211@gmail.com</p>
         </div>
       </div>
     </div>
